283374be5642c05130d058a896351fc6b6299baf
Vitalijs Komarovs <adeukan@gmail.com>
2019-02-07 11:42:48 +0000

    ver_02

--- src/Movie.js
+++ src/Movie.js
@@ -1,40 +1,38 @@
 import React from 'react';
 
 class Movie extends React.Component {
-	constructor(props) {
-		super(props);
-		// setup the state
-		this.state = {
-			key: '',
-			poster_path: 'http://image.tmdb.org/t/p/w185'
-		};
+    constructor(props) {
+        super(props);
+        // setup the state
+        this.state = {
+            key: '',
+            poster_path: 'http://image.tmdb.org/t/p/w185'
+        };
 
-		// bind callback methods with `this`
-		this.handleClick = this.handleClick.bind(this);
-	}
+        // bind callback methods with `this`
+        this.handleClick = this.handleClick.bind(this);
+    }
 
-	// click poster handler
-	handleClick() {
-		this.setState((prevState) => ({})); //// !!!!!!!!!!!!!!!!!!!
-	}
+    // click poster handler
+    handleClick() {
+        this.setState((prevState) => ({})); //// !!!!!!!!!!!!!!!!!!!
+    }
 
-	// runs when component is mounted
-	componentDidMount() {
-		this.setState(
-			(prevState) => ({ poster_path: prevState.poster_path + this.props.poster_id }),
-			() => {
-				console.log(this.state.poster_path);
-			}
-		);
-	}
+    // runs when component is mounted
+    componentDidMount() {
+        this.setState(
+            (prevState) => ({ poster_path: prevState.poster_path + this.props.poster_id }),
+            () => {
+                console.log(this.state.poster_path);
+            }
+        );
+    }
 
-	render() {
-		return (
-			<td>
-				<img onClick={this.handleClick} src={this.state.poster_path} alt="Movie Poster" />
-			</td>
-		);
-	}
+    render() {
+        return (
+            <img onClick={this.handleClick} src={this.state.poster_path} alt="Poster" className="poster col-sm-4 col-md-3 col-lg-2" />
+        );
+    }
 }
 
 export default Movie;
--- src/index.js
+++ src/index.js
@@ -4,53 +4,46 @@ import axios from 'axios';
 import Movie from "./Movie";
 
 class MovieList extends React.Component {
-	constructor(props) {
-		super(props);
-		// this is where we store movies, when they have been retrieved
-		this.state = { movies: [] };
-	}
+    constructor(props) {
+        super(props);
+        // this is where we store movies, when they have been retrieved
+        this.state = { movies: [] };
+    }
 
-	// runs when component is mounted
-	componentDidMount() {
+    // runs when component is mounted
+    componentDidMount() {
 
-		// url query for all popular movies
-		var url =
-			'https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=a3abe9699d800e588cb2a57107b4179c';
-		// get all popular movies from TMDb
-		axios
-			.get(url)
-			.then((response) => {
+        // url query for all popular movies
+        var url =
+            'https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=a3abe9699d800e588cb2a57107b4179c';
+        // get all popular movies from TMDb
+        axios
+            .get(url)
+            .then((response) => {
 
-				// console.log(response.data.results);
+                // console.log(response.data.results);
 
-				// store the movies in state
-				this.setState({ movies: response.data.results });
-				
-			})
-			.catch((err) => {
-				// log the error
-				console.log(err);
-			});
-	}
+                // store the movies in state
+                this.setState({ movies: response.data.results });
 
-	render() {
-		const MovieList = this.state.movies.map( movie => (
-			<Movie key={movie.id} poster_id={movie.poster_path}/>
-		));
+            })
+            .catch((err) => {
+                // log the error
+                console.log(err);
+            });
+    }
 
-		return  (
-            <div class="panel-body">
-              <table class="table table-bordered table-striped table-responsive">
-                <tbody>
-                  <tr><span> Popular Movies </span></tr>
-                  <tr>
-                  	{MovieList}
-                  </tr>
-                </tbody>
-              </table>
+    render() {
+        const MovieList = this.state.movies.slice(0, 12).map(movie => (
+            <Movie key={movie.id} poster_id={movie.poster_path}/>
+        ));
+
+        return (
+            <div className="panel-body wrapper">
+                {MovieList}
             </div>
-            )
-	}
+        )
+    }
 }
 
 ReactDOM.render(<MovieList />, document.getElementById('root'));
